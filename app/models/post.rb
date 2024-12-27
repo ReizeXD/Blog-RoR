ef4330b97d9432405297b8cd5a6f256aad2a9582
ReixeXD <anderson-silva2003@hotmail.com>
2024-12-27 20:48:51 -0300

    Adicionar model comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {maximum: 20}
   validates :content, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    has_many :post, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    
     has_secure_password
+
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     
